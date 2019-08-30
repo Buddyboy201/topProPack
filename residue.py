e2f15f3d9feb680715ace2ef14c2842ac4caaeb0
xperthunter <kjfraga@ucdavis.edu>
2019-08-29 17:24:43 -0700

    Added comments and questions. Updated get_COM and get_centroid.

--- residue.py
+++ residue.py
@@ -2,10 +2,10 @@ import atom
 
 class Residue:
     def __init__(self, name, resid, atoms):
-        self.name = name
-        self.resid = resid
-        self.atoms = atoms
-        self.centroid = None
+        self.name = name # Residue type - prefer single letter code
+        self.resid = resid # ID number for residue in structure
+        self.atoms = atoms # what is atoms? List of atom positions?
+        self.centroid = None # or COM?
 
     def get_atoms(self):
         return self.atoms
@@ -14,7 +14,7 @@ class Residue:
         return self.resid
 
     def get_COM(self):
-        if self.name == "GLY":
+        if self.name == "GLY" or self.name == "G": # Make flexible with single letter or triple letter codes. 
             if self.atoms[1].get_name() == "CA": return self.atoms[1].get_coords()
             else: print("No CA atom found for GLY molecule at {}".format(self.resid))          
         COM = [0.0, 0.0, 0.0]
@@ -27,9 +27,12 @@ class Residue:
         COM[0] /= float(mass_sum)
         COM[1] /= float(mass_sum)
         COM[2] /= float(mass_sum)
+        self.centroid = tuple(COM) # Set centroid attribute. 
         return tuple(COM)
 
     def get_centroid(self):
+        if self.centroid == None
+        self.centroid = self.get_COM
         return self.centroid
 
     def add_atom(self, atm):
@@ -37,5 +40,3 @@ class Residue:
 
     def update_COM(self):
         self.centroid = self.get_COM()
-        
-
