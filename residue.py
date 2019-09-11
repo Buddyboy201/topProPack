3abd0c30a565dcbb2a8f85fea4139e3ad8d4d13a
xperthunter <kjfraga@ucdavis.edu>
2019-09-11 12:19:45 -0700

    Changed how COM for GLY handled.

--- residue.py
+++ residue.py
@@ -15,20 +15,26 @@ class Residue:
 
     def get_COM(self):
         if self.name == "GLY" or self.name == "G": # Make flexible with single letter or triple letter codes. 
-            if self.atoms[1].get_name() == "CA": return self.atoms[1].get_coords()
-            else: print("No CA atom found for GLY molecule at {}".format(self.resid))          
-        COM = [0.0, 0.0, 0.0]
-        mass_sum = 0
-        for atm in self.atoms:
-            COM[0] += atm.get_mass()*atm.get_coords()[0]
-            COM[1] += atm.get_mass()*atm.get_coords()[1]
-            COM[2] += atm.get_mass()*atm.get_coords()[2]
-            mass_sum += atm.get_mass()
-        COM[0] /= float(mass_sum)
-        COM[1] /= float(mass_sum)
-        COM[2] /= float(mass_sum)
-        self.centroid = tuple(COM) # Set centroid attribute. 
-        return tuple(COM)
+            for atm in self.atoms:
+                if atm.get_name() == "CA":
+                    self.centroid = atm.get_coords()
+                    return atm.get_coords()
+            if self.centroid is None:
+                print("No CA atom found for GLY molecule at {}".format(self.resid))
+                return
+        else:    
+            COM = [0.0, 0.0, 0.0]
+            mass_sum = 0
+            for atm in self.atoms:
+                COM[0] += atm.get_mass()*atm.get_coords()[0]
+                COM[1] += atm.get_mass()*atm.get_coords()[1]
+                COM[2] += atm.get_mass()*atm.get_coords()[2]
+                mass_sum += atm.get_mass()
+            COM[0] /= float(mass_sum)
+            COM[1] /= float(mass_sum)
+            COM[2] /= float(mass_sum)
+            self.centroid = tuple(COM) # Set centroid attribute. 
+            return tuple(COM)
 
     def get_centroid(self):
         if self.centroid == None:
